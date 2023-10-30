eee5985088266ecf887d7f08db252ab49d0af488
Resn4R <purview-joust.0x@icloud.com>
2023-10-30 09:56:25 +0000

    added data controller

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -7,17 +7,9 @@
 
 import SwiftUI
 
-struct Strudent: Hashable {
-    let name: String
-}
-
-
 struct ContentView: View {
-    let students = [Strudent(name: "Nice"), Strudent(name: "Holy Moly")]
     var body: some View {
-        List( students, id: \.hashValue) { student in
-            Text(student.name)
-        }
+        Text("Hello, world!")
     }
 }
 
---
+++ CoreDataProject/DataController.swift
@@ -0,0 +1,22 @@
+//
+//  DataController.swift
+//  BookWorm
+//
+//  Created by Vito Borghi on 19/10/2023.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+}
