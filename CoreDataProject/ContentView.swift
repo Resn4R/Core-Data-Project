c3c0bcc4ebe5d83e8d51e6303696d858e09f68ad
Resn4R <purview-joust.0x@icloud.com>
2023-10-30 10:27:50 +0000

    Filtering @FetchRequest using NSPredicate

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -9,24 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment (\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    @FetchRequest (sortDescriptors: [], predicate: NSPredicate(format: "universe == %@", "Star Wars")) var ships: FetchedResults<Ship>
     
     var body: some View {
         VStack {
-            List(wizards, id: \.self) { wizard in
-                Text(wizard.name ?? "Unknown")
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown")
             }
-            
-            Button("Add") {
-                let wizard = Wizard(context: moc)
-                wizard.name = "Harry Potter"
-            }
-            Button("Save") {
-                do {
-                    try moc.save()
-                } catch {
-                    print(error.localizedDescription)
-                }
+            Button("Add Examples") {
+                let ship1 = Ship(context: moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+                
+                let ship2 = Ship(context: moc)
+                ship1.name = "Defiant"
+                ship1.universe = "Star Trek"
+                
+                let ship3 = Ship(context: moc)
+                ship1.name = "Millennium Falcon"
+                ship1.universe = "Star Wars"
+                
+                try? moc.save()
             }
         }
     }
