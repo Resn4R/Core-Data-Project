23030cd94bc1e0fbce222581a5b6ac7f68b7f114
Resn4R <purview-joust.0x@icloud.com>
2023-10-30 09:48:39 +0000

    created core data model, set to manual

---
+++ Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Vito Borghi on 30/10/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
